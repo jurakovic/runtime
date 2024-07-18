021bcf4a6f86e45cf83164bf80c8aec91ce98757
jurakovic <17744091+jurakovic@users.noreply.github.com>
2024-07-18 23:52:42 +0200

    update; move files to this branch

--- build.sh
+++ build.sh
@@ -1,43 +1,48 @@
 #!/bin/bash
 
-#git fetch upstream main
-#git checkout main
-#git rebase upstream/main
-#git push
+function main() {
+
+  #git fetch upstream main
+  #git checkout main
+  #git rebase upstream/main
+  #git push
 
 
-git checkout docs-init
+  git checkout docs-init
 
-rm -rf docs_temp
+  rm -rf docs_temp
 
-curr=$(git log -n 1 --format="%h" --abbrev=40 -- docs/design/coreclr/botr)
-prev=$(git show botr-init:commit.txt)
+  mkdocs_config=$(git show botr-init:mkdocs.yml)
 
-
-if [ ! $curr = $prev ]
-then
-	echo "diff"
-	
-	cd docs/design/coreclr
-
-	mkdocs build --site-dir ../../../docs_temp
-	cp mkdocs.yml ../../../docs_temp/mkdocs.yml
-else
-	echo "no diff"
-fi;
+  curr=$(git log -n 1 --format="%h" --abbrev=40 -- docs/design/coreclr/botr)
+  prev=$(git show botr-init:commit.txt)
 
 
-git checkout botr-init
+  if [ ! $curr = $prev ]
+  then
+  	echo "diff"
 
+  	cd docs/design/coreclr
 
-cd ../../..
+  	echo "$mkdocs_config" | mkdocs build --site-dir ../../../docs_temp -f -
 
-rm -rf docs
-mv docs_temp docs
-mv docs/mkdocs.yml .
+  else
+    echo "no diff"
+  fi;
 
-sed -i "s/docs_dir: 'botr'/docs_dir: 'docs'/g" mkdocs.yml
+  git checkout botr-init
+  cd ../../..
 
-echo "$curr" > commit.txt
+  if [ ! $curr = $prev ]
+  then
 
-git add .
+    rm -rf docs
+    mv docs_temp docs
+
+    echo "$curr" > commit.txt
+
+    #git add .
+  fi;
+}
+
+main

fa4e6ff1eff2075a5a2073c408695108c7d9ff66
jurakovic <17744091+jurakovic@users.noreply.github.com>
2024-07-18 23:01:19 +0200

    update

--- build.sh
+++ build.sh
@@ -8,13 +8,35 @@
 
 git checkout docs-init
 
+rm -rf docs_temp
+
 curr=$(git log -n 1 --format="%h" --abbrev=40 -- docs/design/coreclr/botr)
-prev=$(git show botr:commit)
+prev=$(git show botr-init:commit)
 
 
-if [! $curr = $prev ]
+if [ ! $curr = $prev ]
 then
 	echo "diff"
+	
+	cd docs/design/coreclr
+
+	mkdocs build --site-dir '../../../docs_temp'
+
 else
 	echo "no diff"
 fi;
+
+
+git checkout botr-init
+
+
+cd ../../..
+
+pwd
+ls -l
+
+
+rm -rf docs
+mv docs_temp docs
+
+git add .

4144d47d3c547660d43609f4f442ec133568f28b
jurakovic <17744091+jurakovic@users.noreply.github.com>
2024-07-19 10:53:02 +0200

    Add scripts, docs (#1)

---
+++ README.md
@@ -0,0 +1,8 @@
+
+## The Book of the Runtime Build Repository
+
+https://jurakovic.github.io/runtime/
+
+*Work In Progress*
+
+More details later...
--- build.sh
+++ build.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 
-#git fetch upstream main
-#git checkout main
-#git rebase upstream/main
-#git push
+mkdocs_config=$(git show botr-init:mkdocs.yml)
 
+#curr=$(git log -n 1 --format="%h" --abbrev=40 -- docs/design/coreclr/botr)
+curr=$(curl -s "https://api.github.com/repos/dotnet/runtime/commits?path=docs/design/coreclr/botr&per_page=1" | jq -r '.[0].sha')
+prev=$(git show botr-init:commit.txt)
 
-git checkout docs-init
-
-curr=$(git log -n 1 --format="%h" --abbrev=40 -- docs/design/coreclr/botr)
-prev=$(git show botr:commit)
-
-
-if [! $curr = $prev ]
+if [ ! $curr = $prev ]
 then
-	echo "diff"
+  echo "diff"
+
+  git checkout docs-init
+
+  cd docs/design/coreclr
+
+  rm -rf docs_temp
+  echo "$mkdocs_config" | mkdocs build --site-dir ../../../docs_temp -f -
+
 else
-	echo "no diff"
+  echo "no diff"
+fi;
+
+git checkout botr-init
+cd ../../..
+
+if [ ! $curr = $prev ]
+then
+
+  rm -rf docs
+  mv docs_temp docs
+
+  echo "$curr" > commit.txt
+
+  #git add .
+  #git commit -m "$curr"
 fi;

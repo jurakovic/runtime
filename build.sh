3bcc66b91368c0f86892058baafe9d5d311ed2ed
jurakovic <17744091+jurakovic@users.noreply.github.com>
2024-07-21 00:36:34 +0200

    change repo to dotnet

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 mkdocs_config=$(cat mkdocs.yml)
 
 #curr=$(git log -n 1 --format="%h" --abbrev=40 -- docs/design/coreclr/botr)
-curr=$(curl -s "https://api.github.com/repos/dotnet/runtime/commits?path=docs/design/coreclr/botr&per_page=1" | jq -r '.[0].sha')
-prev="foo" #$(git cat commit.txt)
+curr="foo" #$(curl -s "https://api.github.com/repos/dotnet/runtime/commits?path=docs/design/coreclr/botr&per_page=1" | jq -r '.[0].sha')
+prev="bar" #$(git cat commit.txt)
 
 if [ ! $curr = $prev ]
 then
@@ -30,7 +30,14 @@ then
   rm -rf docs
   mv docs_temp docs
 
-  echo "$curr" > commit.txt
+  # change dotnet repo to fork; fix js api
+  find docs -type f -iwholename "*.html" -exec sed -i -r 's/(href="https:\/\/github.com\/)(dotnet)(\/runtime" title="Go to repository")/\1jurakovic\3/' {} +
+  find docs/assets/javascripts -type f -iwholename "*.min.js" -exec sed -i -r 's/(https:\/\/api.github.com\/repos\/)(\$\{e\}\/\$\{t\})/\1dotnet\/runtime/' {} +
+
+  # change view raw url
+  find docs -type f -iwholename "*.html" -exec sed -i -r 's/(href="https:\/\/github\.com\/dotnet\/runtime\/)(raw)(.*" title="View source of this page")/\1blob\3/' {} +
+
+  #echo "$curr" > commit.txt
 
   #git add .
   #git commit -m "$curr"

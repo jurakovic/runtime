a5be2162e8526d2922b97047504940031fbe0433
jurakovic <17744091+jurakovic@users.noreply.github.com>
2024-07-22 19:28:51 +0200

    update build script; extract check to separate script

---
+++ check.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#curr=$(git log -n 1 --format="%h" --abbrev=40 -- docs/design/coreclr/botr)
+curr=$(curl -s "https://api.github.com/repos/dotnet/runtime/commits?path=docs/design/coreclr/botr&per_page=1" | jq -r '.[0].sha')
+prev=$(cat commit.txt)
+
+if [ $curr = $prev ]
+then
+  echo "There are no changes"
+  read -p "Continue with build anyway? (y/n) " yn
+else
+  echo "There are changes"
+  read -p "Continue with build? (y/n) " yn
+fi
+
+if [ -z "$yn" ] || [ "$yn" != "y" ]; then exit 0; fi;
+
+./build.sh
+
+echo "$curr" > commit.txt

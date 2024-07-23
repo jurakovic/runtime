063175edbebbf1f1297c13064a69b141e1ff19ef
jurakovic <17744091+jurakovic@users.noreply.github.com>
2024-07-23 16:50:17 +0200

    Include ryujit files, minor build.sh optimizations (#5)

--- build.sh
+++ build.sh
@@ -3,22 +3,23 @@
 # save current branch
 branch=$(git branch --show-current)
 
-# save config for later
-mkdocs_config=$(cat mkdocs.yml)
-
 echo "Checking out 'main' branch"
 git checkout main
 
 # clear any leftovers
 rm -rf site
 
-# temp file
-echo "$mkdocs_config" > mkdocs.yml
+# temp config
+echo "$(git show $branch:mkdocs.yml)" > mkdocs.yml
+
+# copy out-of-scope files
+cp docs/design/coreclr/botr/../jit/ryujit-overview.md docs/design/coreclr/botr/ryujit-overview.md
+cp docs/design/coreclr/botr/../jit/porting-ryujit.md docs/design/coreclr/botr/porting-ryujit.md
 
 # hide toc on all pages (no other proposed solutions work)
 mapfile -t files < <(find docs/design/coreclr/botr -type f -iwholename "*.md")
 for file in "${files[@]}"; do
-  sed -i '1s;^;---\nhide:\n  - toc\n---\n;' "$file"
+  sed -i '1s/^/---\nhide:\n  - toc\n---\n/' "$file"
 done
 
 echo "Staring mkdocs build"
@@ -28,6 +29,8 @@ docker run --rm -it -v ${PWD}:/docs mkdocs-botr build
 
 # undoing temp changes
 rm mkdocs.yml
+rm docs/design/coreclr/botr/ryujit-overview.md
+rm docs/design/coreclr/botr/porting-ryujit.md
 git restore '*.md'
 
 echo "Checking out '$branch' branch"
@@ -40,22 +43,24 @@ rm -rf docs
 mv site docs
 
 # change dotnet repo to fork; fix api url
-find docs -type f -iwholename "*.html" -exec sed -i -r 's/(href="https:\/\/github.com\/)(dotnet)(\/runtime" title="Go to repository")/\1jurakovic\3/' {} +
-find docs/assets/javascripts -type f -iwholename "*.min.js" -exec sed -i -r 's/(https:\/\/api.github.com\/repos\/)(\$\{e\}\/\$\{t\})/\1dotnet\/runtime/' {} +
+find docs -type f -iwholename "*.html" -exec sed -i -r 's|(href="https://github.com/)(dotnet)(/runtime" title="Go to repository")|\1jurakovic\3|' {} +
+find docs/assets/javascripts -type f -iwholename "*.min.js" -exec sed -i -r 's|(https://api.github.com/repos/)(\$\{e\}/\$\{t\})|\1dotnet/runtime|' {} +
 
 # change view url from raw to github
-find docs -type f -iwholename "*.html" -exec sed -i -r 's/(href="https:\/\/github\.com\/dotnet\/runtime\/)(raw)(.*" title="View source of this page")/\1blob\3/' {} +
+find docs -type f -iwholename "*.html" -exec sed -i -r 's|(href="https://github\.com/dotnet/runtime/)(raw)(.*" title="View source of this page")|\1blob\3 target="_blank"|' {} +
+sed -i -r 's|(href="https://github\.com/dotnet/runtime/blob/main/docs/design/coreclr/)(botr)(.*" title="View source of this page")|\1jit\3"|' docs/ryujit-overview/*.html docs/porting-ryujit/*.html
 
-# fix chapters url
-sed -i -r 's/(href=")(..\/botr)(">All Book of the Runtime \(BOTR\) chapters on GitHub)/\1https:\/\/github.com\/dotnet\/runtime\/blob\/main\/docs\/design\/coreclr\/botr" target="_blank\3/' docs/index.html
+# fix index urls
+sed -i -r 's|(href=")(../botr)(">All Book of the Runtime \(BOTR\) chapters on GitHub)|\1https://github.com/dotnet/runtime/blob/main/docs/design/coreclr/botr" target="_blank\3|' docs/index.html
+sed -i -r 's|(../jit/)(.*)(.md)|\2/|g' docs/index.html
 
 # add footer url
-text='in <a href="https://github.com/jurakovic/runtime" target="_blank" rel="noopener">jurakovic/runtime</a>'
+text='in <a href="https://github.com/jurakovic/runtime" target="_blank">jurakovic/runtime</a>'
 mapfile -t files < <(find docs -type f -iwholename "*.html")
 for file in "${files[@]}"; do
   total_lines=$(wc -l < "$file")
   insert_line=$((total_lines - 44))
-  if [ "$insert_line" -gt 0 ]; then sed -i "${insert_line}i$text" "$file"; fi
+  sed -i "${insert_line}i$text" "$file"
 done
 
 echo "Done"

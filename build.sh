226c1c9ce0b6d9e756f7f822e16ec8980adc7245
jurakovic <17744091+jurakovic@users.noreply.github.com>
2024-07-23 08:15:50 +0200

    sed updates

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ cp docs/design/coreclr/botr/../jit/porting-ryujit.md docs/design/coreclr/botr/po
 # hide toc on all pages (no other proposed solutions work)
 mapfile -t files < <(find docs/design/coreclr/botr -type f -iwholename "*.md")
 for file in "${files[@]}"; do
-  sed -i '1s;^;---\nhide:\n  - toc\n---\n;' "$file"
+  sed -i '1s/^/---\nhide:\n  - toc\n---\n/' "$file"
 done
 
 echo "Staring mkdocs build"
@@ -46,23 +46,23 @@ rm -rf docs
 mv site docs
 
 # change dotnet repo to fork; fix api url
-find docs -type f -iwholename "*.html" -exec sed -i -r 's/(href="https:\/\/github.com\/)(dotnet)(\/runtime" title="Go to repository")/\1jurakovic\3/' {} +
-find docs/assets/javascripts -type f -iwholename "*.min.js" -exec sed -i -r 's/(https:\/\/api.github.com\/repos\/)(\$\{e\}\/\$\{t\})/\1dotnet\/runtime/' {} +
+find docs -type f -iwholename "*.html" -exec sed -i -r 's|(href="https://github.com/)(dotnet)(/runtime" title="Go to repository")|\1jurakovic\3|' {} +
+find docs/assets/javascripts -type f -iwholename "*.min.js" -exec sed -i -r 's|(https://api.github.com/repos/)(\$\{e\}/\$\{t\})|\1dotnet/runtime|' {} +
 
 # change view url from raw to github
-find docs -type f -iwholename "*.html" -exec sed -i -r 's/(href="https:\/\/github\.com\/dotnet\/runtime\/)(raw)(.*" title="View source of this page")/\1blob\3/' {} +
+find docs -type f -iwholename "*.html" -exec sed -i -r 's|(href="https://github\.com/dotnet/runtime/)(raw)(.*" title="View source of this page")|\1blob\3 target="_blank"|' {} +
 
 # fix index urls
-sed -i -r 's/(href=")(..\/botr)(">All Book of the Runtime \(BOTR\) chapters on GitHub)/\1https:\/\/github.com\/dotnet\/runtime\/blob\/main\/docs\/design\/coreclr\/botr" target="_blank\3/' docs/index.html
+sed -i -r 's|(href=")(../botr)(">All Book of the Runtime \(BOTR\) chapters on GitHub)|\1https://github.com/dotnet/runtime/blob/main/docs/design/coreclr/botr" target="_blank\3|' docs/index.html
 sed -i -r 's|(../jit/)(.*)(.md)|\2/|g' docs/index.html
 
 # add footer url
-text='in <a href="https://github.com/jurakovic/runtime" target="_blank" rel="noopener">jurakovic/runtime</a>'
+text='in <a href="https://github.com/jurakovic/runtime" target="_blank">jurakovic/runtime</a>'
 mapfile -t files < <(find docs -type f -iwholename "*.html")
 for file in "${files[@]}"; do
   total_lines=$(wc -l < "$file")
   insert_line=$((total_lines - 44))
-  if [ "$insert_line" -gt 0 ]; then sed -i "${insert_line}i$text" "$file"; fi
+  sed -i "${insert_line}i$text" "$file"
 done
 
 echo "Done"

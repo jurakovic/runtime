13539e8853be10efb16032649ea9f8c81fd41e1f
jurakovic <17744091+jurakovic@users.noreply.github.com>
2024-07-23 08:35:20 +0200

    fix jit gh links

--- build.sh
+++ build.sh
@@ -48,6 +48,7 @@ find docs/assets/javascripts -type f -iwholename "*.min.js" -exec sed -i -r 's|(
 
 # change view url from raw to github
 find docs -type f -iwholename "*.html" -exec sed -i -r 's|(href="https://github\.com/dotnet/runtime/)(raw)(.*" title="View source of this page")|\1blob\3 target="_blank"|' {} +
+sed -i -r 's|(href="https://github\.com/dotnet/runtime/blob/main/docs/design/coreclr/)(botr)(.*" title="View source of this page")|\1jit\3"|' docs/ryujit-overview/*.html docs/porting-ryujit/*.html
 
 # fix index urls
 sed -i -r 's|(href=")(../botr)(">All Book of the Runtime \(BOTR\) chapters on GitHub)|\1https://github.com/dotnet/runtime/blob/main/docs/design/coreclr/botr" target="_blank\3|' docs/index.html

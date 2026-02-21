703a6bd9e5988e08ec33be2af50368421abf18c2
jurakovic <17744091+jurakovic@users.noreply.github.com>
2026-02-22 00:24:59 +0100

    Update theme colors and typography (#12)

--- build.sh
+++ build.sh
@@ -1,75 +1,95 @@
 #!/bin/bash
 
-echo "Creating new 'dotnet' worktree with 'main' branch"
-git worktree add dotnet main
-cd dotnet
-git pull
+docs_dir='docs/design/coreclr/botr'
 
-# clear any leftovers
-rm -rf site
+function add_worktree {
+  echo "Adding new 'dotnet' worktree with 'main' branch"
+  git worktree add dotnet main
+  cd dotnet
+  git pull
+}
 
-# temp config
-cp ../mkdocs.yml .
+function copy_mkdocs_config {
+  cp ../mkdocs.yml .
+  cp ../custom.css $docs_dir/custom.css
+  mkdir -p overrides/partials
+  cp ../favicon.svg $docs_dir/favicon.svg
+  cp ../copyright.html overrides/partials/copyright.html
+}
 
-# copy out-of-scope files
-cp docs/design/coreclr/botr/../jit/ryujit-overview.md docs/design/coreclr/botr/ryujit-overview.md
-cp docs/design/coreclr/botr/../jit/porting-ryujit.md docs/design/coreclr/botr/porting-ryujit.md
+function pre_build {
+  # copy out-of-scope files
+  cp $docs_dir/../jit/ryujit-overview.md $docs_dir/ryujit-overview.md
+  cp $docs_dir/../jit/porting-ryujit.md $docs_dir/porting-ryujit.md
 
-# fix profiling.md; e.g. List<int> => List&lt;int&gt;
-sed -i -r 's/(\w+)(<)([a-zA-Z,]+)(>)/\1\&lt;\3\&gt;/g' docs/design/coreclr/botr/profiling.md
+  # fix profiling.md; e.g. List<int> => List&lt;int&gt;
+  sed -i -r 's/(\w+)(<)([a-zA-Z,]+)(>)/\1\&lt;\3\&gt;/g' $docs_dir/profiling.md
 
-# temp fix to images path in jit file
-sed -i -r 's;]\(images;]\(https://raw.githubusercontent.com/dotnet/runtime/refs/heads/main/docs/design/coreclr/jit/images;g' docs/design/coreclr/botr/ryujit-overview.md
+  # temp fix to images path in jit file
+  sed -i -r 's;]\(images;]\(https://raw.githubusercontent.com/dotnet/runtime/refs/heads/main/docs/design/coreclr/jit/images;g' $docs_dir/ryujit-overview.md
 
-mapfile -t files < <(find docs/design/coreclr/botr -type f -iwholename "*.md")
-for file in "${files[@]}"; do
-  # hide toc on all pages (no other proposed solutions work)
-  sed -i '1s/^/---\nhide:\n  - toc\n---\n/' "$file"
-  # update links to jit files, because they are copied to botr dir (cp commands above)
-  sed -i -r 's;(\.\.\/jit\/)(.*\.md);\2;g' "$file"
-  # change relative links for out-of-scope files to github
-  sed -i -r 's;(\(|]: )\.\./\.\./\.\./\.\./;\1https://github.com/dotnet/runtime/blob/main/;g' "$file"
-  sed -i -r 's;(\(|]: )\.\./\.\./\.\./;\1https://github.com/dotnet/runtime/blob/main/docs/;g' "$file"
-  sed -i -r 's;(\(|]: )\.\./\.\./;\1https://github.com/dotnet/runtime/blob/main/docs/design/;g' "$file"
-  sed -i -r 's;(\(|]: )\.\./;\1https://github.com/dotnet/runtime/blob/main/docs/design/coreclr/;g' "$file"
-done
+  mapfile -t files < <(find $docs_dir -type f -iwholename "*.md")
+  for file in "${files[@]}"; do
+    # hide toc on all pages (no other proposed solutions work)
+    sed -i '1s/^/---\nhide:\n  - toc\n---\n/' "$file"
+    # update links to jit files, because they are copied to botr dir (cp commands above)
+    sed -i -r 's;(\.\.\/jit\/)(.*\.md);\2;g' "$file"
+    # change relative links for out-of-scope files to github
+    sed -i -r 's;(\(|]: )\.\./\.\./\.\./\.\./;\1https://github.com/dotnet/runtime/blob/main/;g' "$file"
+    sed -i -r 's;(\(|]: )\.\./\.\./\.\./;\1https://github.com/dotnet/runtime/blob/main/docs/;g' "$file"
+    sed -i -r 's;(\(|]: )\.\./\.\./;\1https://github.com/dotnet/runtime/blob/main/docs/design/;g' "$file"
+    sed -i -r 's;(\(|]: )\.\./;\1https://github.com/dotnet/runtime/blob/main/docs/design/coreclr/;g' "$file"
+  done
+}
 
-echo "Staring mkdocs build"
-docker run --rm -v ${PWD}:/docs ghcr.io/jurakovic/mkdocs-botr:latest build </dev/null
-#for debugging:
-#docker run --rm -it -v ${PWD}:/docs --entrypoint /bin/sh ghcr.io/jurakovic/mkdocs-botr:latest
+function build {
+  echo "Staring mkdocs build"
 
-# back to repo root
-cd ..
+  # clear any leftovers
+  rm -rf site
 
-# clear old build
-rm -rf docs
+  docker run --rm -v ${PWD}:/docs ghcr.io/jurakovic/mkdocs-botr:latest build </dev/null
+  #for debugging:
+  #docker run --rm -it -v ${PWD}:/docs --entrypoint /bin/sh ghcr.io/jurakovic/mkdocs-botr:latest
 
-# move to docs dir
-mv dotnet/site docs
-rm -rf dotnet
+  # back to repo root
+  cd ..
 
-# change dotnet repo to fork; fix api url
-find docs -type f -iwholename "*.html" -exec sed -i -r 's|(href="https://github.com/)(dotnet)(/runtime" title="Go to repository")|\1jurakovic\3|' {} +
-find docs/assets/javascripts -type f -iwholename "*.min.js" -exec sed -i -r 's|(https://api.github.com/repos/)(\$\{e\}/\$\{t\})|\1dotnet/runtime|' {} +
+  # clear old build
+  rm -rf docs
 
-# change view url from raw to github
-find docs -type f -iwholename "*.html" -exec sed -i -r 's|(href="https://github\.com/dotnet/runtime/)(raw)(.*" title="View source of this page")|\1blob\3 target="_blank"|' {} +
-sed -i -r 's|(href="https://github\.com/dotnet/runtime/blob/main/docs/design/coreclr/)(botr)(.*" title="View source of this page")|\1jit\3"|' docs/ryujit-overview/*.html docs/porting-ryujit/*.html
+  # move to docs dir
+  mv dotnet/site docs
+}
 
-# fix index urls
-sed -i -r 's|(">All Book of the Runtime \(BOTR\) chapters on GitHub)|" target="_blank\1|' docs/index.html
+function post_build {
+  # change dotnet repo to fork; fix api url
+  find docs -type f -iwholename "*.html" -exec sed -i -r 's|(href="https://github.com/)(dotnet)(/runtime" title="Go to repository")|\1jurakovic\3|' {} +
+  find docs/assets/javascripts -type f -iwholename "*.min.js" -exec sed -i -r 's|(https://api.github.com/repos/)(\$\{e\}/\$\{t\})|\1dotnet/runtime|' {} +
 
-# add footer url
-text='in <a href="https://github.com/jurakovic/runtime" target="_blank">jurakovic/runtime</a>'
-mapfile -t files < <(find docs -type f -iwholename "*.html")
-for file in "${files[@]}"; do
-  total_lines=$(wc -l < "$file")
-  insert_line=$((total_lines - 48))
-  sed -i "${insert_line}i$text" "$file"
-done
+  # change view url from raw to github
+  find docs -type f -iwholename "*.html" -exec sed -i -r 's|(href="https://github\.com/dotnet/runtime/)(raw)(.*" title="View source of this page")|\1blob\3 target="_blank"|' {} +
+  sed -i -r 's|(href="https://github\.com/dotnet/runtime/blob/main/docs/design/coreclr/)(botr)(.*" title="View source of this page")|\1jit\3"|' docs/ryujit-overview/*.html docs/porting-ryujit/*.html
 
-echo "Removing 'dotnet' worktree"
-git worktree remove dotnet --force
+  # fix index urls
+  sed -i -r 's|(">All Book of the Runtime \(BOTR\) chapters on GitHub)|" target="_blank\1|' docs/index.html
+}
 
-echo "Done"
+function remove_worktree {
+  echo "Removing 'dotnet' worktree"
+  rm -rf dotnet
+  git worktree remove dotnet --force
+}
+
+function main {
+  #cd dotnet
+  add_worktree
+  copy_mkdocs_config
+  pre_build
+  build
+  post_build
+  remove_worktree
+  echo "Done"
+}
+
+main
--- check.sh
+++ check.sh
@@ -4,6 +4,11 @@
 curr=$(curl -s "https://api.github.com/repos/dotnet/runtime/commits?path=docs/design/coreclr/botr&per_page=1" | jq -r '.[0].sha')
 prev=$(cat commit.txt)
 
+if [ -z "$curr" ] || [ "$curr" = "null" ]; then
+  echo "Failed to fetch latest commit"
+  exit 1
+fi
+
 if [ $curr = $prev ]
 then
   echo "There are no changes"
@@ -17,3 +22,5 @@ if [ -z "$yn" ] || [ "$yn" != "y" ]; then exit 1; fi;
 ./build.sh
 
 echo "$curr" > commit.txt
+
+git diff --name-only $prev $curr -- docs/design/coreclr/botr
